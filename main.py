9bfdc33a83efa480ef53659b0e7d3cd563a280b1
Matheus Gaudencio <matheusgr@gmail.com>
2012-12-02 23:24:46 -0300

    Added allowed users control.

--- main.py
+++ main.py
@@ -15,6 +15,21 @@ class ExecScript(db.Model):
     code = db.TextProperty(required=True)
     name = db.StringProperty(required=True)
 
+class AllowedUser(db.Model):
+    email = db.StringProperty(required=True)
+
+
+def deny_access(response):
+    if not users.is_current_user_admin():
+        user = users.get_current_user()
+        allowed_user = AllowedUser.all().filter('email =', user.email()).get()
+        if not allowed_user:
+            response.set_status(403)
+            response.out.write("You should have permission to access this page")
+            return True
+    return False
+
+
 class Exec(webapp2.RequestHandler):
     def get(self):
         machines_dict = memcache.get('machines') or {}
@@ -32,8 +47,31 @@ class Exec(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.out.write(script)
 
+class AdminUser(webapp2.RequestHandler):
+    def get(self):
+        allowed_users = AllowedUser.all().order('email')
+        template_values = {
+            'users': allowed_users,
+        }
+        path = 'users.html'
+        self.response.out.write(template.render(path, template_values))
+
+class AdminUserNew(webapp2.RequestHandler):
+    def post(self):
+        email = self.request.get('email')
+        AllowedUser(email=email).put()
+        self.redirect('/admin/user')
+
+class AdminUserEdit(webapp2.RequestHandler):
+    def post(self):
+        key = self.request.get('key')
+        db.delete(key)
+        self.redirect('/admin/user')
+
 class Admin(webapp2.RequestHandler):
     def get(self):
+        if deny_access(self.response):
+            return
         scripts = ExecScript.all()
         template_values = {
             'scripts': scripts,
@@ -43,6 +81,8 @@ class Admin(webapp2.RequestHandler):
 
 class AdminExecNew(webapp2.RequestHandler):
     def post(self):
+        if deny_access(self.response):
+            return
         code = '\n'.join(self.request.get('code').splitlines())
         name = self.request.get('name')
         ExecScript(code=code, name=name).put()
@@ -50,6 +90,8 @@ class AdminExecNew(webapp2.RequestHandler):
 
 class AdminExecEdit(webapp2.RequestHandler):
     def post(self):
+        if deny_access(self.response):
+            return
         name = self.request.get('name')
         code = '\n'.join(self.request.get('code').splitlines())
         key = self.request.get('key')
@@ -63,7 +105,17 @@ class AdminExecEdit(webapp2.RequestHandler):
 
 class Index(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write('Online...<br><br>')
+        user = users.get_current_user()
+        if not user:
+            self.response.out.write("<a href=%s>Login</a><br><br>" % users.create_login_url())
+            return
+        else:
+            if deny_access(self.response):
+                self.response.out.write("<br><br>User %s - <a href=%s>Logout</a><br>" % (user.email(), users.create_logout_url('/')))
+                return
+            self.response.out.write("User %s - <a href=%s>Logout</a><br>" % (user.email(), users.create_logout_url('/')))
+            self.response.out.write("<a href=/admin>Edit exec script</a><br>")
+            self.response.out.write("<a href=/admin/user>Edit allowed users</a><br><br>")
         machines_dict = memcache.get('machines')
         if not machines_dict:
             self.response.out.write('None')
@@ -82,6 +134,9 @@ app = webapp2.WSGIApplication([
   ('/', Index),
   ('/exec', Exec),
   ('/admin', Admin),
+  ('/admin/user', AdminUser),
+  ('/admin/useredit', AdminUserEdit),
+  ('/admin/usernew', AdminUserNew),
   ('/admin/execedit', AdminExecEdit),
   ('/admin/execnew', AdminExecNew),
   ], debug=DEBUG)
