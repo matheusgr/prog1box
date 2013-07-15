23b78d19068f2a9f4185f738f72d5e5af5cdb403
Matheus Gaudencio do Rêgo <matheusgr@cuica.gmf.ufcg.edu.br>
2013-07-15 16:45:13 -0300

    Added script used 2012_2.

---
+++ script_2012_2.sh
@@ -0,0 +1,144 @@
+# Reboot?
+#reboot
+
+# Pagina inicial:
+echo 'http://dl.dropbox.com/u/27356397/inicio.html' > /tmp/pagina
+
+echo "nameserver 8.8.8.8
+nameserver 8.8.4.4" > /etc/resolv.conf
+
+# Se a URL comecar igual a uma dessas linhas, libere o acesso:
+echo "https://www.google.com/a/ccc.ufcg.edu.br/ServiceLogin?
+https://dl.dropboxusercontent.com/u/74287933/2012.2/sim.html
+https://dl.dropboxusercontent.com/u/74287933/2012.2/lista.html
+http://dl.dropboxusercontent.com/u/74287933/2012.2/sim.html
+http://dl.dropboxusercontent.com/u/74287933/2012.2/lista.html
+http://dl.dropboxusercontent.com/u/74287933/2012.2/reports/last-results.html
+http://dl.dropboxusercontent.com/u/74287933/2012.2/reports/last-results.txt
+http://tstview.appspot.com/
+https://dl.dropboxusercontent.com/u/74287933/2012.2/soma_polinomios.png
+http://dl.dropboxusercontent.com/u/74287933/2012.2/soma_polinomios.png
+http://download-webshell.appspot.com/
+http://www.download-webshell.appspot.com/
+http://bit.ly/prog1roteiro7
+https://docs.google.com/document/pub?id=1Phm5BTJtNMqaEh6-fuEnZOj7qXDKTXpIKW49Ggxtp1Y
+http://dl.dropbox.com/u/9427789/2012.2/roteiro7.pdf
+http://dl.dropbox.com/u/9427789/2012.2/reports/
+http://dl.dropbox.com/u/9427789/2012.1/last-results.html
+http://dl.dropbox.com/u/9427789/2012.1/last-results.txt
+http://dl.dropbox.com/u/9427789/2012.2/reports/index.html
+http://dl.dropbox.com/u/9427789/2012.2/reports/prog1.css
+https://dl.dropbox.com/u/74287933/2012.2/
+http://bit.ly/exercicios20122
+http://bit.ly/roteiro7b
+http://bit.ly/122sev5
+http://dl.dropbox.com/u/9427789/2012.2/lista.html
+http://bit.ly/exercicios20122
+http://bit.ly/exercicios20122b
+https://www.google.com/a/ccc.ufcg.edu.br/LoginAction2?
+https://www.google.com/accounts/
+https://www.google.com.br/accounts/
+https://ssl.gstatic.com/
+https://gg.google.com/
+https://accounts.google.com/
+https://accounts.youtube.com/
+https://appengine.google.com/_ah/
+https://accounts.google.com.br/
+http://dl.dropbox.com/u/9427789/2012.2/l1.html
+http://dl.dropbox.com/u/9427789/2012.2/l2.html
+http://dl.dropbox.com/u/9427789/2012.2/mtp1.html
+http://dl.dropbox.com/u/27356397/inicio.html
+http://dl.dropbox.com/u/27356397/roteiro3.pdf
+http://dl.dropbox.com/u/27356397/154.txt
+http://bit.ly/prog1roteiro1
+https://docs.google.com/document/pub?id=1ytu7npkv6CGybqPA7Av0cTlkf5jUaUbDcmvhv_6vXks
+https://docs.google.com/a/ccc.ufcg.edu.br/document/pub?id=1ytu7npkv6CGybqPA7Av0cTlkf5jUaUbDcmvhv_6vXks
+https://docs.google.com/document/pubimage?id=1ytu7npkv6CGybqPA7Av0cTlkf5jUaUbDcmvhv_6vXks
+http://bit.ly/prog1roteiro2
+https://docs.google.com/document/pub?id=1YN6jReWZvvmy6uUnJeOqGx1qJnYhox3fFfYRaI5rmto
+https://docs.google.com/a/ccc.ufcg.edu.br/document/pub?id=1YN6jReWZvvmy6uUnJeOqGx1qJnYhox3fFfYRaI5rmto
+https://docs.google.com/document/pubimage?id=1YN6jReWZvvmy6uUnJeOqGx1qJnYhox3fFfYRaI5rmto
+http://bit.ly/prog1roteiro3
+https://docs.google.com/document/pub?id=1nbWEx8D9nzKnCXm6fGoOy5xn0KLe27uKk3AP7iS_v5s
+https://docs.google.com/a/ccc.ufcg.edu.br/document/pub?id=1nbWEx8D9nzKnCXm6fGoOy5xn0KLe27uKk3AP7iS_v5s
+https://docs.google.com/document/pubimage?id=1nbWEx8D9nzKnCXm6fGoOy5xn0KLe27uKk3AP7iS_v5s
+http://bit.ly/prog1roteiro4
+https://docs.google.com/document/pub?id=1peMHHgvfZPQ9_ODRpOJWRxUoSWPvAmnit3aDOCr7Nmc
+https://docs.google.com/document/pubimage?id=1peMHHgvfZPQ9_ODRpOJWRxUoSWPvAmnit3aDOCr7Nmc
+http://bit.ly/prog1roteiro5
+https://docs.google.com/document/pub?id=1Q-ovIj5DBgzqL5NNQ3v_WNoP2Dp981l1mxUY0zqExeU
+https://docs.google.com/document/pubimage?id=1Q-ovIj5DBgzqL5NNQ3v_WNoP2Dp981l1mxUY0zqExeU
+http://python-webshell.appspot.com/
+https://python-webshell.appspot.com/
+https://docs.google.com/document/d/1zgc5AV1cd6lf0SqIRbiapVQ9-rtUbDqdycLEB8dexF4/pub
+http://bit.ly/prog1roteiro8b
+https://docs.google.com/document/pub?id=1yDEZYLA_nJSJZGPw8h82dIRFbKUFtH_pdmSmcvujT-Q
+https://docs.google.com/document/pubimage?id=1yDEZYLA_nJSJZGPw8h82dIRFbKUFtH_pdmSmcvujT-Q
+https://docs.google.com/document/d/1yDEZYLA_nJSJZGPw8h82dIRFbKUFtH_pdmSmcvujT-Q/pub
+http://dl.dropbox.com/u/27356397/roteiro9.pdf
+http://bit.ly/119XmuQ
+https://dl.dropbox.com/u/37343177/sequencia-collatz5.jpg
+https://dl.dropbox.com/u/74287933/2012.2/lista.html
+http://dl.dropbox.com/u/74287933/2012.2/lista.html
+http://dl.dropbox.com/u/74287933/2012.2/reports/last-results.html
+http://dl.dropbox.com/u/74287933/2012.2/last-results.html
+http://dl.dropbox.com/u/74287933/2012.2/last-results.txt
+http://dl.dropbox.com/u/74287933/2012.2/reports/index.html
+http://dl.dropbox.com/u/74287933/2012.2/reports/prog1.css
+http://dl.dropbox.com/u/74287933/2012.2/reports/last-results.txt
+http://bit.ly/prog1mapas
+http://docs.google.com/document/d/1nJuC7h6ay61gnamA4kf67q1Dwa2wW5PD7iW7QtRy6pA/pub
+https://docs.google.com/document/d/1nJuC7h6ay61gnamA4kf67q1Dwa2wW5PD7iW7QtRy6pA/pub
+https://dl.dropbox.com/u/74287933/2012.2/roteiro_mapas.pdf
+http://bit.ly/prog1mapaspdf" > /tmp/sites
+
+# Sites liberados durante o TOPPL (mudar o destino para /tmp/sites)
+echo "https://www.google.com/a/ccc.ufcg.edu.br/ServiceLogin?
+https://dl.dropboxusercontent.com/u/74287933/2012.2/lista.html
+https://dl.dropboxusercontent.com/u/74287933/2012.2/sim.html
+http://dl.dropboxusercontent.com/u/74287933/2012.2/lista.html
+http://dl.dropboxusercontent.com/u/74287933/2012.2/sim.html
+http://bit.ly/exercicios20122b
+https://dl.dropbox.com/u/74287933/08f0beeafb40af41350278dfa9a69863.html
+http://dl.dropbox.com/u/74287933/08f0beeafb40af41350278dfa9a69863.html
+http://dl.dropbox.com/u/27356397/inicio.html
+http://dl.dropbox.com/u/9427789/2012.1/
+http://dl.dropbox.com/u/9427789/2012.2/
+http://dl.dropbox.com/u/74287933/2012.2/
+https://www.google.com/a/ccc.ufcg.edu.br/LoginAction2?
+https://www.google.com/accounts/
+https://www.google.com.br/accounts/
+https://ssl.gstatic.com/
+https://gg.google.com/
+https://accounts.google.com/
+https://accounts.youtube.com/
+https://appengine.google.com/_ah/
+https://accounts.google.com.br/
+http://python-webshell.appspot.com/
+https://python-webshell.appspot.com/" > /tmp/x-sites
+
+# Proibindo pendrives
+echo "blacklist usb-storage" > /etc/modprobe.d/blacklist.conf
+rmmod usb-storage
+
+# Desativando o acesso ao CD
+chmod 700 /media
+
+# Desativando lock screen e log-out
+sleep 1
+su aluno -c 'DISPLAY=:0 dconf write /org/gnome/desktop/lockdown/disable-lock-screen true'
+sleep 1
+su aluno -c 'DISPLAY=:0 dconf write /org/gnome/desktop/lockdown/disable-log-out false'
+sleep 1
+su aluno -c 'DISPLAY=:0 dconf write /org/gnome/desktop/lockdown/disable-user-switching true'
+sleep 1
+
+# Desativando o screensaver
+su aluno -c 'DISPLAY=:0 dconf write /org/gnome/desktop/screensaver/lock-enabled false'
+sleep 1
+
+# Configurando proxy para o aluno
+su aluno -c 'DISPLAY=:0 dconf write /system/proxy/http/host \"localhost\"'
+sleep 1
+su aluno -c 'DISPLAY=:0 dconf write /system/proxy/mode \"manual\"'
+sleep 1
